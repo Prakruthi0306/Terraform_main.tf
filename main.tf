3bd1786d58b9b7c0afa28d5c684d857bd5e77799
Prakruthi0306 <110379361+Prakruthi0306@users.noreply.github.com>
2022-12-07 13:08:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,58 @@ terraform {
     }
   }
 }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.34.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example1-resources0306"
+  location = "West US"
+}
+
+resource "azurerm_container_registry" "example" {
+  name                = "containerRegistry1999910306"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Premium"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks123610306"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks123"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "azurerm_role_assignment" "example" {
+  principal_id                     = azurerm_kubernetes_cluster.example.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.example.id
+  skip_service_principal_aad_check = true
+}
 provider "azurerm" {
   features {}
 }
